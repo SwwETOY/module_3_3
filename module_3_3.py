eae9716def4a4193f2fa219813f5964d1619c26f
SwwETOY <swwetoy@rambler.ru>
2024-10-02 18:46:49 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,13 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+values_list = [5, 'СТРОКА', True]
+values_dict = {55, 100, 33}
+print_params(*values_list)
+print_params(*values_dict)
+print(*values_list, *values_dict)
+values_list_2 = [54.32, 'СтРоКа']
+print_params(*values_list_2, 42)
