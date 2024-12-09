32d94e89fb226c59abe01d0ad1d6fa24fe5a993d
SwwETOY <swwetoy@rambler.ru>
2024-12-09 06:51:21 +0300

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -1,13 +1,22 @@
-def print_params(a = 1, b = 'строка', c = True):
-    print(a, b, c)
+def print_params(a=1, b='строка', c=True):
+    print(f"a = {a}")
+    print(f"b = {b}")
+    print(f"c = {c}")
 
-print_params(b = 25)
-print_params(c = [1,2,3])
+# Вызовы функции с разным количеством аргументов
+print_params()  # Вызов без аргументов
+print_params(5)  # Вызов с одним аргументом
+print_params(5, 'другая строка')  # Вызов с двумя аргументами
+print_params(5, 'другая строка', False)  # Вызов со всеми аргументами
+print_params(b=25)  # Вызов с ключевым аргументом b
+print_params(c=[1, 2, 3])  # Вызов с ключевым аргументом c
 
-values_list = [5, 'СТРОКА', True]
-values_dict = {55, 100, 33}
-print_params(*values_list)
-print_params(*values_dict)
-print(*values_list, *values_dict)
-values_list_2 = [54.32, 'СтРоКа']
-print_params(*values_list_2, 42)
+values_list = [10, 'новый текст', False]
+values_dict = {'a': 20, 'b': 'еще один текст', 'c': True}
+
+print_params(*values_list)  # Распаковка списка
+print_params(**values_dict)  # Распаковка словаря
+
+values_list_2 = [30, 'последний текст']
+
+print_params(*values_list_2, c=42)  # Распаковка списка + аргумент
